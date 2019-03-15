3c86cb0b304a11076c96c2b0268409aa2a3f1c3b
bluedevil97 <xd2015_xd@hotmail.com>
2019-03-15 13:03:14 -0500

    menu y iconos

--- resources/views/admin/producto.blade.php
+++ resources/views/admin/producto.blade.php
@@ -25,6 +25,7 @@
     <link rel="stylesheet" type="text/css" href="/css/ofrecemos.css">
       <link rel="stylesheet" type="text/css" href="/css/estilos.css">
 
+     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script> 
     <title>Web landing - Practica</title>
   </head>
   <body>
@@ -38,31 +39,46 @@
              
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <img src="img/imagenes/logo.png" class="logo">
-                <form class="form-inline my-2 my-lg-0 ">
+                <!--<form class="form-inline my-2 my-lg-0 ">
                   <input class="form-control mr-sm-2 buscar" type="search" placeholder="Search" aria-label="Search">
-                </form>
+                </form>-->
                 <ul class="navbar-nav mr-auto">
                   
                   <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="#"><i class="fab fa-dropbox iconos"></i><br>Productos</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-boxes iconos"></i><br>Inventario</a>
                   </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Dropdown
+
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-boxes iconos"></i><br>Inventario</a>
+                  </li> 
+
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-boxes iconos"></i><br>Inventario</a>
+                  </li>
+
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-boxes iconos"></i><br>Inventario</a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-boxes iconos"></i><br>Inventario</a>
+                  </li>
+
+                  <li class="nav-item dropdown active">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-user iconos2"></i><i class="fas fa-angle-down iconos2"></i><br>
+                      Usuario
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                       <!-- <div class="dropdown-divider"></div>-->
+                        <a class="dropdown-item" href="#">Something else here</a>
+                      </div>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                  </li>
+                 
                 </ul>
                 
               </div>
