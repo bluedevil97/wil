4594ed88e97c6c911744c02bf57e1ad525e791b7
bluedevil97 <xd2015_xd@hotmail.com>
2019-03-15 08:41:03 -0500

    menu

---
+++ resources/views/admin/producto.blade.php
@@ -0,0 +1,389 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+
+    <!-- MI HOJA DE ESTILSO-->
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+
+    <link rel="stylesheet" type="text/css" href="ofrecemos.css">
+
+    <!-- Mis fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">
+     <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="/css/ofrecemos.css">
+      <link rel="stylesheet" type="text/css" href="/css/estilos.css">
+
+    <title>Web landing - Practica</title>
+  </head>
+  <body>
+    
+
+    <!--Navbar -->
+   
+          <div class="menu">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark menu2">
+             
+             
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <img src="img/imagenes/logo.png" class="logo">
+                <form class="form-inline my-2 my-lg-0 ">
+                  <input class="form-control mr-sm-2 buscar" type="search" placeholder="Search" aria-label="Search">
+                </form>
+                <ul class="navbar-nav mr-auto">
+                  
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>
+                </ul>
+                
+              </div>
+            </nav>
+          </div> 
+            
+   
+
+<!-- HEADER -->
+   
+
+     <!-- TEAM -->
+    <!--<section class="text-center team">
+      <div class="container p-5">
+        <h1 class="text-center text-dark">Team</h1>
+        <p class="text-dark">
+          Adipisicing unde eum odio aliquam dolor ad voluptas expedita consequuntur aperiam inventore eaque. Dolorum facilis aut eum a delectus. Fugit velit quas harum quisquam amet provident inventore. Repudiandae repudiandae delectus.<br><br><br><br>
+        </p>
+        <div class="row">
+
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <img src="img/img1.png" class="img-fluid rounded-circle w-50">
+                <h3>John Carter</h3>
+                <p>
+                  Dolor modi repudiandae quia beatae consectetur? Nam ullafugit ullam, accusamus! Totam mollitia eveniet!
+                </p>
+                <div class="d-flex flex-row justify-content-center">
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  </div>
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                  </div>
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <img src="img/img2.png" class="img-fluid rounded-circle w-50">
+                <h3>John Carter</h3>
+                <p>
+                  Dolor modi repudiandae quia beatae consectetur? Nam ullafugit ullam, accusamus! Totam mollitia eveniet!
+                </p>
+                <div class="d-flex flex-row justify-content-center">
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  </div>
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                  </div>
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <img src="img/imagenes/img3.png" class="img-fluid rounded-circle w-50">
+                <h3>John Carter</h3>
+                <p>
+                  Dolor modi repudiandae quia beatae consectetur? Nam ullafugit ullam, accusamus! Totam mollitia eveniet!
+                </p>
+                <div class="d-flex flex-row justify-content-center">
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  </div>
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                  </div>
+                  <div class="p-4">
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>-->
+
+
+  <!-- Product -->
+    <div class="product">
+      <div class="container">
+      <h1 class="text-center text-dark">Product</h1>
+        <p class="text-dark text-center">
+          Adipisicing unde eum odio aliquam dolor ad voluptas expedita consequuntur aperiam inventore eaque. Dolorum facilis aut eum a delectus. Fugit velit quas harum quisquam amet provident inventore. Repudiandae repudiandae delectus.<br><br><br><br>
+        </p>
+      <div class="row">
+        <div class="col-sm-4 pt-4">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Cumpleaños</h5>
+                  
+                </div>
+
+                <div class="contenedor-img ejemplo-1">  
+                         <img class="card-img-top tamaño" src="img/imagenes/producto1.jpg" alt="Card image cap">
+                       <div class="mascara">  
+                        
+                           <p><strong>Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo
+                           Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo.</strong></p>
+                           <a href="#" class="btn btn-outline-danger btn-lg text-white link">ver catalogo</a>
+                       </div>  
+                  </div>
+
+               
+               <!-- 
+                <div class="card-footer justify-content-center">
+                  <p class="text-muted justify-content-center">Precio: 
+                    <strong>S/. 100.00 </strong></p>
+                  <a href="#" class="btn btn-danger left " >Comprar</a>
+                 </div>
+                -->
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 pt-4">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Matrimonios</h5>
+                  
+                </div>
+
+                <div class="contenedor-img ejemplo-1">  
+                         <img class="card-img-top tamaño" src="img/matri.jpg" alt="Card image cap">
+                       <div class="mascara">  
+                        
+                           <p><strong>Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo
+                           Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo.</strong></p>
+                           <a href="#" class="btn btn-outline-danger btn-lg text-white link">ver catalogo</a>
+                       </div>  
+                  </div>
+
+               
+               <!-- 
+                <div class="card-footer justify-content-center">
+                  <p class="text-muted justify-content-center">Precio: 
+                    <strong>S/. 100.00 </strong></p>
+                  <a href="#" class="btn btn-danger left " >Comprar</a>
+                 </div>
+                -->
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 pt-4">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Bautizo</h5>
+                  
+                </div>
+
+                <div class="contenedor-img ejemplo-1">  
+                         <img class="card-img-top tamaño" src="img/bautizo.jpg" alt="Card image cap">
+                       <div class="mascara">  
+                        
+                           <p><strong>Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo
+                           Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo.</strong></p>
+                           <a href="#" class="btn btn-outline-danger btn-lg text-white link">ver catalogo</a>
+                       </div>  
+                  </div>
+
+               
+               <!-- 
+                <div class="card-footer justify-content-center">
+                  <p class="text-muted justify-content-center">Precio: 
+                    <strong>S/. 100.00 </strong></p>
+                  <a href="#" class="btn btn-danger left " >Comprar</a>
+                 </div>
+                -->
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 pt-4">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Card title</h5>
+                  
+                </div>
+
+                <div class="contenedor-img ejemplo-1">  
+                         <img class="card-img-top tamaño" src="img/cumple.jpg" alt="Card image cap">
+                       <div class="mascara">  
+                        
+                           <p><strong>Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo
+                           Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo.</strong></p>
+                           <a href="#" class="btn btn-outline-danger btn-lg text-white link">ver catalogo</a>
+                       </div>  
+                  </div>
+
+               
+               <!-- 
+                <div class="card-footer justify-content-center">
+                  <p class="text-muted justify-content-center">Precio: 
+                    <strong>S/. 100.00 </strong></p>
+                  <a href="#" class="btn btn-danger left " >Comprar</a>
+                 </div>
+                -->
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 pt-4">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Card title</h5>
+                  
+                </div>
+
+                <div class="contenedor-img ejemplo-1">  
+                         <img class="card-img-top tamaño" src="img/cumple.jpg" alt="Card image cap">
+                       <div class="mascara">  
+                        
+                           <p><strong>Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo
+                           Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo.</strong></p>
+                           <a href="#" class="btn btn-outline-danger btn-lg text-white link">ver catalogo</a>
+                       </div>  
+                  </div>
+
+               
+               <!-- 
+                <div class="card-footer justify-content-center">
+                  <p class="text-muted justify-content-center">Precio: 
+                    <strong>S/. 100.00 </strong></p>
+                  <a href="#" class="btn btn-danger left " >Comprar</a>
+                 </div>
+                -->
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 pt-4">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Card title</h5>
+                  
+                </div>
+
+                <div class="contenedor-img ejemplo-1">  
+                         <img class="card-img-top tamaño" src="img/cumple.jpg" alt="Card image cap">
+                       <div class="mascara">  
+                        
+                           <p><strong>Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo
+                           Proveemos servicios y equipos a nivel coorporativo. Puede buscar mas informacion en el enlace de abajo.</strong></p>
+                           <a href="#" class="btn btn-outline-danger btn-lg text-white link">ver catalogo</a>
+                       </div>  
+                  </div>
+
+               
+               <!-- 
+                <div class="card-footer justify-content-center">
+                  <p class="text-muted justify-content-center">Precio: 
+                    <strong>S/. 100.00 </strong></p>
+                  <a href="#" class="btn btn-danger left " >Comprar</a>
+                 </div>
+                -->
+
+            </div>
+          </div>
+        </div>
+
+
+      </div> 
+    </div>
+    </div>
+  <!-- FOOTER-->
+    <footer>
+      <div class="row footer">
+        <div class="col text-center">
+          <p>2018 All Rights Reserverd by Joel</p>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
+</html>
